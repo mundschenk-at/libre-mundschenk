ceb09e21eae518b50ea645bceed890f1f95ca418
Der Mundschenk & Compagnie <github@mundschenk.at>
2016-03-29 21:24:50 +0200

    Add namespace to function name in callback

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 namespace Mundschenk\Libre;
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'Mundschenk\Libre\theme_enqueue_styles' );
 function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
