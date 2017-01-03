4b5dc4d5aab86a423a38e91d99a65da16582e3c5
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-01-03 23:09:09 +0100

    Ignore parser errors

--- functions.php
+++ functions.php
@@ -7,3 +7,5 @@ function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+add_filter( 'typo_ignore_parser_errors', '__return_true' );
